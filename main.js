d51bf48cc1e36d9523e7e22b019e13bf2606b038
Taurus0205 <dolchin1990@mail.ru>
2021-08-19 22:04:05 +0500

    Add three templates to html

--- main.js
+++
@@ -1,6 +0,0 @@
-const elHumbergerBtn = document.querySelector(".hamburger-btn");
-const elHeader = document.querySelector(".header");
-
-elHumbergerBtn.addEventListener("click", function () {
-  elHeader.classList.toggle("header--adoptive");
-});
